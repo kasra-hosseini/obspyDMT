60171fa72d7b0f2b35dd3b223970b961c4abebc7
kasra-hosseini <hosseini@geophysik.uni-muenchen.de>
2015-02-18 18:32:10 +0100

    testing event_handler

---
+++ obspyDMT/tests/test_event_handler.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# -------------------------------------------------------------------
+#   Filename:  test_event_handler.py
+#   Purpose:   testing event_handler
+#   Author:    Kasra Hosseini
+#   Email:     hosseini@geophysik.uni-muenchen.de
+#   License:   GPLv3
+# -------------------------------------------------------------------
+
+# -----------------------------------------------------------------------
+# ----------------Import required Modules (Python and Obspy)-------------
+# -----------------------------------------------------------------------
+
+# Required Python and Obspy modules will be imported in this part.
+from obspy.core import UTCDateTime
+
+from obspyDMT.utils.input_handler import command_parse, read_input_command
+from obspyDMT.utils.event_handler import gcmt_catalog
+
+
+def test_gmt_catalog():
+    (options, args, parser) = command_parse()
+    input_dics = read_input_command(parser)
+    input_dics['min_date'] = UTCDateTime('2011-03-01')
+    input_dics['max_date'] = UTCDateTime('2011-03-20')
+    input_dics['min_mag'] = 8.9
+
+    evlatmin = input_dics['evlatmin']
+    evlatmax = input_dics['evlatmax']
+    evlonmin = input_dics['evlonmin']
+    evlonmax = input_dics['evlonmax']
+
+    evlat = input_dics['evlat']
+    evlon = input_dics['evlon']
+    evradmax = input_dics['evradmax']
+    evradmin = input_dics['evradmin']
+
+    events_QML = \
+        gcmt_catalog(input_dics['min_date'],
+                     input_dics['max_date'],
+                     evlatmin, evlatmax, evlonmin, evlonmax,
+                     evlat, evlon, evradmin, evradmax,
+                     input_dics['min_depth'],
+                     input_dics['max_depth'],
+                     input_dics['min_mag'],
+                     input_dics['max_mag'])
+    assert events_QML[0].preferred_origin().latitude == 37.520
+    assert events_QML[0].preferred_origin().longitude == 143.050
+    assert events_QML[0].preferred_origin().depth == 20000.
