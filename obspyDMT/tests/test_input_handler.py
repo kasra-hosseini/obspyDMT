da34a0b16ea35f88411f45d3eacd40a4b1b34437
kasra-hosseini <hosseini@geophysik.uni-muenchen.de>
2015-03-02 14:32:38 +0100

    update test

--- obspyDMT/tests/test_input_handler.py
+++ obspyDMT/tests/test_input_handler.py
@@ -39,8 +39,8 @@ def test_read_input_command():
     assert len(parser.option_groups[9].option_list) == 14
     assert len(parser.option_groups[10].option_list) == 3
     assert len(parser.option_groups[11].option_list) == 7
-    assert len(parser.option_groups[12].option_list) == 17
-    assert len(parser.option_groups[13].option_list) == 14
+    assert len(parser.option_groups[12].option_list) == 18
+    assert len(parser.option_groups[13].option_list) == 13
     assert len(parser.option_groups[14].option_list) == 3
 
     input_dics = read_input_command(parser)
@@ -48,13 +48,15 @@ def test_read_input_command():
 
     return input_dics
 
+
 def test_default_inputs():
     input_dics = test_read_input_command()
     assert os.path.basename(input_dics['datapath']) == 'obspyDMT-data'
-    assert (UTCDateTime(input_dics['max_date']) - UTCDateTime(input_dics['min_date']) < (60 * 60 * 24 * 5 + 1))
+    assert (UTCDateTime(input_dics['max_date']) -
+            UTCDateTime(input_dics['min_date']) < (60 * 60 * 24 * 5 + 1))
     assert input_dics['event_url'] == 'IRIS'
-    assert input_dics['event_catalog'] == None
-    assert input_dics['mag_type'] == None
+    assert input_dics['event_catalog'] == 'GCMT_COMBO'
+    assert input_dics['mag_type'] is None
     assert input_dics['min_mag'] == 5.5
     assert input_dics['max_mag'] == 9.9
     assert input_dics['min_depth'] == -10.0
@@ -64,14 +66,14 @@ def test_default_inputs():
     assert input_dics['preset_cont'] == 0 
     assert input_dics['offset_cont'] == 0
     assert input_dics['req_np'] == 4
-    assert input_dics['list_stas'] == False
+    assert input_dics['list_stas'] is False
     assert input_dics['waveform'] == 'Y'
     assert input_dics['response'] == 'Y'
     assert input_dics['FDSN'] == 'Y'
     assert input_dics['ArcLink'] == 'N'
     assert input_dics['fdsn_base_url'] == 'IRIS'
-    assert input_dics['fdsn_user'] == None
-    assert input_dics['fdsn_pass'] == None
+    assert input_dics['fdsn_user'] is None
+    assert input_dics['fdsn_pass'] is None
     assert input_dics['arc_avai_timeout'] == 40  
     assert input_dics['arc_wave_timeout'] == 2  
     assert input_dics['SAC'] == 'Y'  
@@ -81,25 +83,25 @@ def test_default_inputs():
     assert input_dics['sta'] == '*'   
     assert input_dics['loc'] == '*'   
     assert input_dics['cha'] == '*'  
-    assert input_dics['evlatmin'] == None   
-    assert input_dics['evlatmax'] == None  
-    assert input_dics['evlonmin'] == None   
-    assert input_dics['evlonmax'] == None  
-    assert input_dics['evlat'] == None   
-    assert input_dics['evlon'] == None  
-    assert input_dics['evradmin'] == None   
-    assert input_dics['evradmax'] == None  
+    assert input_dics['evlatmin'] is None
+    assert input_dics['evlatmax'] is None
+    assert input_dics['evlonmin'] is None
+    assert input_dics['evlonmax'] is None
+    assert input_dics['evlat'] is None
+    assert input_dics['evlon'] is None
+    assert input_dics['evradmin'] is None
+    assert input_dics['evradmax'] is None
     assert input_dics['max_result'] == 2500  
     assert input_dics['depth_bins_seismicity'] == 10  
-    assert input_dics['lat_cba'] == None   
-    assert input_dics['lon_cba'] == None  
-    assert input_dics['mr_cba'] == None   
-    assert input_dics['Mr_cba'] == None  
-    assert input_dics['mlat_rbb'] == None   
-    assert input_dics['Mlat_rbb'] == None  
-    assert input_dics['mlon_rbb'] == None   
-    assert input_dics['Mlon_rbb'] == None  
-    assert input_dics['test'] == 'N'  
+    assert input_dics['lat_cba'] is None
+    assert input_dics['lon_cba'] is None
+    assert input_dics['mr_cba'] is None
+    assert input_dics['Mr_cba'] is None
+    assert input_dics['mlat_rbb'] is None
+    assert input_dics['Mlat_rbb'] is None
+    assert input_dics['mlon_rbb'] is None
+    assert input_dics['Mlon_rbb'] is None
+    assert input_dics['test'] is 'N'
     assert input_dics['fdsn_update'] == 'N'   
     assert input_dics['arc_update'] == 'N'   
     assert input_dics['update_all'] == 'N'  
@@ -122,22 +124,21 @@ def test_default_inputs():
     assert input_dics['arc_merge_auto'] == 'N'  
     assert input_dics['plot_all'] == 'Y'  
     assert input_dics['plot_type'] == 'raw'  
-    assert input_dics['plot_ev'] == 'N'   
-    assert input_dics['plot_sta'] == 'N'   
-    assert input_dics['plot_se'] == 'N'  
-    assert input_dics['plot_ray'] == 'N'  
-    assert input_dics['plot_epi'] == 'N'   
-    assert input_dics['plot_dt'] == 'N'  
-    assert input_dics['plot_ray_gmt'] == 'N'  
+    assert input_dics['plot_ev'] is False
+    assert input_dics['plot_sta'] is False
+    assert input_dics['plot_ray'] is False
+    assert input_dics['plot_epi'] is False
+    assert input_dics['plot_dt'] is False
+    assert input_dics['plot_ray_gmt'] is False
     assert os.path.basename(input_dics['plot_save']) == '.'   
     assert input_dics['plot_format'] == 'png'  
     assert input_dics['min_epi'] == 0.0   
     assert input_dics['max_epi'] == 180.0  
-    assert input_dics['plotxml_dir'] == False  
-    assert input_dics['plotxml_date'] == False  
+    assert input_dics['plotxml_dir'] is False
+    assert input_dics['plotxml_date'] is False
     assert input_dics['plotxml_min_freq'] == 0.01  
     assert input_dics['plotxml_output'] == 'VEL'  
     assert input_dics['plotxml_start_stage'] == 1  
     assert input_dics['plotxml_end_stage'] == 100  
     assert input_dics['plotxml_percentage'] == 80  
-    assert input_dics['plotxml_phase_threshold'] == 0.1  
+    assert input_dics['plotxml_phase_threshold'] == 10.
