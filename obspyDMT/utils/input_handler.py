2e63d8bc2c65df8822f26242c540b876e42f8ccf
kasra-hosseini <hosseini@geophysik.uni-muenchen.de>
2015-03-04 14:25:02 +0100

    changed the version to 0.9.0

--- obspyDMT/utils/input_handler.py
+++ obspyDMT/utils/input_handler.py
@@ -848,7 +848,7 @@ def read_input_command(parser, **kwargs):
     if options.version:
         print '\n\t\t' + '*********************************'
         print '\t\t' + '*        obspyDMT version:      *'
-        print '\t\t' + '*' + '\t\t' + '0.8.0' + '\t\t' + '*'
+        print '\t\t' + '*' + '\t\t' + '0.9.0' + '\t\t' + '*'
         print '\t\t' + '*********************************'
         print '\n'
         sys.exit(2)
