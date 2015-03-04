5f9420dde83a822ee26163a08a1969f3700ee33a
kasra-hosseini <hosseini@geophysik.uni-muenchen.de>
2015-03-04 14:24:46 +0100

    updated the setup.py

--- README.txt
+++ README.txt
@@ -5,15 +5,11 @@ Copyright
 ---------
 GNU Lesser General Public License, Version 3 (LGPLv3)
 
-Copyright (c) 2012-2014 by:
+Copyright (c) 2012-2015 by:
     * Kasra Hosseini
 
 Overview
 --------
-obspyDMT (ObsPy Data Management Tool) is a command line tool for retrieving, 
-processing and management of massive seismological data in a fully automatic way 
-which could be run in serial or in parallel. 
-Moreover, complementary processing and managing tools have been designed 
-and introduced in addition to the obspyDMT options. 
-Because of the modular nature, different functionalities could be added easily 
-and/or each scripts can be used as a module for other programs.
+obspyDMT (obspy Data Management Tool) is a command line tool for retrieving,
+processing and management of large seismological datasets in a fully automatic
+way which can be run in serial or in parallel.
--- setup.py
+++ setup.py
@@ -2,24 +2,22 @@ from setuptools import setup
 #from distutils.core import setup
 
 setup(
-    name = "obspyDMT",
-    version = "0.7.6",
-    description = "Retrieving, Processing and Management of Massive Seismological Data (Serial and Parallel)",
-    author = "Kasra Hosseini",
-    author_email = "hosseini@geophysik.uni-muenchen.de",
-    url = "https://github.com/kasra-hosseini/obspyDMT",
-    download_url = "https://github.com/kasra-hosseini/obspyDMT.git",
-    keywords = ["obspyDMT", "ObsPy", "Seismology"],
+    name="obspyDMT",
+    version="0.9.0",
+    description="obspyDMT: A Python Toolbox for Retrieving and Processing of Large Seismological Datasets",
+    author="Kasra Hosseini",
+    author_email="hosseini@geophysik.uni-muenchen.de",
+    url="https://github.com/kasra-hosseini/obspyDMT",
+    download_url="https://github.com/kasra-hosseini/obspyDMT.git",
+    keywords=["obspyDMT", "ObsPy", "Seismology"],
     packages=["obspyDMT"],
-    entry_points = {
+    requires=['matplotlib', 'numpy'],
+    entry_points={
         'console_scripts': [
             'obspyDMT = obspyDMT.obspyDMT:main',
-            'compareDMT = obspyDMT.compareDMT:main',
-            'obspyDMT_managing_node = obspyDMT.obspyDMT_managing_node:main',
-            'obspyNC = obspyDMT.obspyNC:main',
         ],
     },
-    classifiers = [
+    classifiers=[
         "Programming Language :: Python",
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
@@ -27,13 +25,9 @@ setup(
         "Operating System :: OS Independent",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-    long_description = """\
-obspyDMT (ObsPy Data Management Tool) is a command line tool for retrieving, 
-processing and management of massive seismological data in a fully automatic way 
-which could be run in serial or in parallel. 
-Moreover, complementary processing and managing tools have been 
-designed and introduced in addition to the obspyDMT options. 
-Because of the modular nature, different functionalities could be added 
-easily and/or obspyDMT can be used as a module for other programs.
+    long_description="""\
+obspyDMT (obspy Data Management Tool) is a command line tool for retrieving,
+processing and management of large seismological datasets in a fully automatic
+way which can be run in serial or in parallel.
 """
 )
