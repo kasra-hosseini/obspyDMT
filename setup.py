c5bad10665f2a31ad71b2557b9bbe2b5955584b9
kasra-hosseini <hosseini@geophysik.uni-muenchen.de>
2015-07-10 01:47:42 +0200

    version 0.9.9d * resample is now added

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ lib = MyExtension('obspyDMT',
 
 setup_config = dict(
     name="obspyDMT",
-    version="0.9.9c",
+    version="0.9.9d",
     keywords=["obspyDMT", "ObsPy", "Seismology"],
     requires=['matplotlib', 'numpy'],
     description="obspyDMT: A Python Toolbox for Retrieving and Processing of "
