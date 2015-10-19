0af2b67a3c7b9fbe2ae671753bc816955e1db8d9
kasra-hosseini <hosseini@geophysik.uni-muenchen.de>
2015-10-19 17:22:42 +0200

    instrument handler is added to apply instrument correction

---
+++ obspyDMT/utils/instrument_handler.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# -------------------------------------------------------------------
+#   Filename:  instrument_handler.py
+#   Purpose:   handling instrument correction in obspyDMT
+#   Author:    Kasra Hosseini
+#   Email:     hosseini@geophysik.uni-muenchen.de
+#   License:   GNU Lesser General Public License, Version 3
+# -------------------------------------------------------------------
+
+# -----------------------------------------------------------------------
+# ----------------Import required Modules (Python and Obspy)-------------
+# -----------------------------------------------------------------------
+from obspy import read_inventory
+try:
+    from obspy.io.xseed import Parser
+except:
+    from obspy.xseed import Parser
+import os
+
+# ##################### instrument_correction #################################
+
+
+def instrument_correction(tr, target_path, save_path, corr_unit, pre_filt,
+                          water_level):
+    """
+    find STXML or DATALESS file for one trace and apply instrument correction
+    :param tr:
+    :param target_path:
+    :param save_path:
+    :param corr_unit:
+    :param pre_filt:
+    :param water_level:
+    :return:
+    """
+    resp_file = os.path.join(target_path, 'resp', 'DATALESS.%s' % tr.id)
+    stxml_file = os.path.join(target_path, 'resp', 'STXML.%s' % tr.id)
+
+    if os.path.isfile(stxml_file):
+        tr_corr = obspy_fullresp_stxml(tr, stxml_file, save_path,
+                                       corr_unit, pre_filt, water_level,
+                                       debug=False)
+    elif os.path.isfile(resp_file):
+        tr_corr = obspy_fullresp_resp(tr, resp_file, save_path,
+                                      corr_unit, pre_filt, water_level,
+                                      debug=False)
+    else:
+        print "%s -- StationXML or Response file does not exist!" % tr.id
+        tr_corr = False
+    return tr_corr
+
+# ##################### obspy_fullresp_stxml #################################
+
+
+def obspy_fullresp_stxml(trace, stxml_file, save_path, unit,
+                         bp_filter, water_level, debug=False):
+    """
+    apply instrument correction using StationXML file
+    :param trace:
+    :param stxml_file:
+    :param save_path:
+    :param unit:
+    :param bp_filter:
+    :param water_level:
+    :param debug:
+    :return:
+    """
+    if 'dis' in unit.lower():
+        unit = 'DISP'
+    elif 'vel' in unit.lower():
+        unit = 'VEL'
+    elif 'acc' in unit.lower():
+        unit = 'ACC'
+    else:
+        unit = unit.upper()
+    try:
+        if debug:
+            print 20*'='
+            print 'stationXML file: %s' % stxml_file
+            print 'tarce: %s' % trace.id
+            print 'save path: %s' % save_path
+
+        if os.path.isfile(save_path):
+            return
+        # remove the trend
+        trace.detrend('linear')
+        inv = read_inventory(stxml_file, format="stationxml")
+        trace.attach_response(inv)
+        trace.remove_response(output=unit,
+                              water_level=water_level,
+                              pre_filt=eval(bp_filter), zero_mean=True,
+                              taper=True, taper_fraction=0.05)
+
+        # Remove the following line to keep the units
+        # as it is in the stationXML
+        # trace.data *= 1.e9
+
+        if unit.lower() == 'disp':
+            unit_print = 'displacement'
+        elif unit.lower() == 'vel':
+            unit_print = 'velocity'
+        elif unit.lower() == 'acc':
+            unit_print = 'acceleration'
+        else:
+            unit_print = 'UNKNOWN'
+        print 'instrument correction to %s for: %s' % (unit_print, trace.id)
+
+        return trace
+
+    except Exception as error:
+        print '[EXCEPTION] %s -- %s' % (trace.id, error)
+        return False
+
+# ##################### obspy_fullresp_resp ##################################
+
+
+def obspy_fullresp_resp(trace, resp_file, save_path, unit,
+                        bp_filter, water_level, debug=False):
+    """
+    apply instrument correction by using response file
+    :param trace:
+    :param resp_file:
+    :param save_path:
+    :param unit:
+    :param bp_filter:
+    :param water_level:
+    :param debug:
+    :return:
+    """
+    if 'dis' in unit.lower():
+        unit = 'DIS'
+    elif 'vel' in unit.lower():
+        unit = 'VEL'
+    elif 'acc' in unit.lower():
+        unit = 'ACC'
+    else:
+        unit = unit.upper()
+
+    dataless_parser = Parser(resp_file)
+    seedresp = {'filename': dataless_parser, 'units': unit}
+
+    if debug:
+        print 20*'='
+        print 'stationXML file: %s' % resp_file
+        print 'tarce: %s' % trace.id
+        print 'save path: %s' % save_path
+
+    # remove the trend
+    trace.detrend('linear')
+    try:
+        trace.simulate(seedresp=seedresp, paz_remove=None, paz_simulate=None,
+                       remove_sensitivity=True, simulate_sensitivity=False,
+                       water_level=water_level,
+                       zero_mean=True, taper=True,
+                       taper_fraction=0.05, pre_filt=eval(bp_filter),
+                       pitsasim=False, sacsim=True)
+
+        # Remove the following line since we want to keep
+        # the units as it is in the stationXML
+        # trace.data *= 1.e9
+
+        if unit.lower() == 'dis':
+            unit_print = 'displacement'
+        elif unit.lower() == 'vel':
+            unit_print = 'velocity'
+        elif unit.lower() == 'acc':
+            unit_print = 'acceleration'
+        else:
+            unit_print = 'UNKNOWN'
+        print 'instrument correction to %s for: %s' % (unit_print, trace.id)
+
+        return trace
+
+    except Exception as error:
+        print '[EXCEPTION] %s -- %s' % (trace.id, error)
+        return False
