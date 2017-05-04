f3f1ff81895060591b08b8154d9873e6ea3c3afd
kasra-hosseini <hosseini@geophysik.uni-muenchen.de>
2017-05-04 15:48:16 +0100

    v2.0.1

--- obspyDMT/utils/input_handler.py
+++ obspyDMT/utils/input_handler.py
@@ -827,7 +827,7 @@ def read_input_command(parser, **kwargs):
     if options.version:
         print('\n\t\t' + '*********************************')
         print('\t\t' + '*        obspyDMT version:      *')
-        print('\t\t' + '*\t' + 5*' ' + '2.0.0' + '\t\t*')
+        print('\t\t' + '*\t' + 5*' ' + '2.0.1' + '\t\t*')
         print('\t\t' + '*********************************')
         print('\n')
         sys.exit(2)
