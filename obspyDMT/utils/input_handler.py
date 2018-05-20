76a02dd60b9a7e6df3eff22baeb55ca7deda8dd3
Kasra Hosseini <kasrash@hal-9000.home>
2018-05-20 20:17:47 +0100

    change to v2.1.1

--- obspyDMT/utils/input_handler.py
+++ obspyDMT/utils/input_handler.py
@@ -838,7 +838,7 @@ def read_input_command(parser, **kwargs):
     if options.version:
         print('\n\t\t' + '*********************************')
         print('\t\t' + '*        obspyDMT version:      *')
-        print('\t\t' + '*\t' + 5*' ' + '2.1.0' + '\t\t*')
+        print('\t\t' + '*\t' + 5*' ' + '2.1.1' + '\t\t*')
         print('\t\t' + '*********************************')
         print('\n')
         sys.exit(2)
